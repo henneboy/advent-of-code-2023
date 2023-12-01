14c88a57094ff4e0363b1046277d17f236ade63b
henneboy <72788863+henneboy@users.noreply.github.com>
2023-12-01 15:20:25 +0100

    Update first.hs

--- Day-1/first.hs
+++ Day-1/first.hs
@@ -1,13 +1,18 @@
+import System.Win32 (xBUTTON1)
+import Data.Char
 
 main = do
     input <- readFile "input.txt"
-    putStrLn input
+    putStrLn [firstAndLastDigit line | line <- split input '\n']
 
+splitStringOnChar :: [a] -> a -> [[a]]
+splitStringOnChar [] char = [[]]
+splitStringOnChar (x:xs) char = if x == char then [x] : (l:ls) else (x : l) : ls
+    where (l:ls) = splitStringOnChar xs char
 
-digits = [0,1,2,3,4,5,6,7,8,9]
-findDigits str = [x | x <- str,  (read x) elem digits]
+findDigits str = [digitToInt x | x <- str, isDigit x]
 
-firstAndLastDigit str = head ds + head (reverse ds)
+firstAndLastDigit str = head ds + last ds
     where
     ds = findDigits str
 
